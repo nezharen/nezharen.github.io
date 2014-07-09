544bf51a5bec25e7c30d6d95f20e12c8da177291
nezharen <nezharen@163.com>
2014-07-09 22:00:27 +0800

    ajax homework

--- share/1/ajax.js
+++ share/1/ajax.js
@@ -1,14 +1,65 @@
-var newsCount = 1;
+var newsNum, newsCount, remarkPageNum, remarkPageCount, timer;
 
-function processData(data) {
-	$("#image-session").css("background-image", data.img);
+function processNewsData(data) {
+	newsNum = data.news.length;
+	$("#image").attr("src", data.news[newsCount].img);
 }
 
 function gotoNews(x) {
+	newsCount = x;
 	$.ajax({
-		url: "json/" + x + ".json",
-		success: processData,
+		url: "json/news.json",
+		success: processNewsData,
 		dataType: "json"
-	})
+	});
+	clearTimeout(timer);
+	timer = setTimeout("gotoNextNews()", 3000);
+}
+
+function gotoNextNews() {
+	if (newsCount < (newsNum - 1))
+		gotoNews(newsCount + 1);
+	else
+		gotoNews(0);
+}
+
+function gotoPreviousNews() {
+	if (newsCount > 0)
+		gotoNews(newsCount - 1);
+	else
+		gotoNews(newsNum - 1);
+}
+
+function processRemarkPageData(data) {
+	remarkPageNum = data.remarkPages.length;
+	
+}
+
+function gotoRemarkPage(x) {
+	remarkPageCount = x;
+	$.ajax({
+		url: "json/remark.json",
+		success: processRemarkPageData,
+		dataType: "json"
+	});
+}
+
+function gotoNextRemarkPage() {
+	if (remarkPageCount < (remarkPageNum - 1))
+		gotoRemarkPage(remarkPageCount + 1);
+	else
+		gotoRemarkPage(0);
+}
+
+function gotoPreviousRemarkPage() {
+	if (remarkPageCount > 0)
+		gotoNews(remarkPageCount - 1);
+	else
+		gotoNews(remarkPageNum - 1);
+}
+
+function getReady() {
+	gotoNews(0);
+	gotoRemarkPage(0);
 }
 
