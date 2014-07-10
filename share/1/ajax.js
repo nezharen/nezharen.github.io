75586e39dea1db2cdf941cd6fc0d49cd892ea363
nezharen <nezharen@163.com>
2014-07-10 16:42:12 +0800

    photo news

--- share/1/ajax.js
+++ share/1/ajax.js
@@ -2,7 +2,9 @@ var newsNum, newsCount, remarkPageNum, remarkPageCount, timer;
 
 function processNewsData(data) {
 	newsNum = data.news.length;
-	$("#image").attr("src", data.news[newsCount].img);
+	$("img#image").attr("src", data.news[newsCount].img);
+	$("div#image-caption").text(data.news[newsCount].caption);
+	$("a.image-link").attr("href", data.news[newsCount].href);
 }
 
 function gotoNews(x) {
@@ -13,7 +15,7 @@ function gotoNews(x) {
 		dataType: "json"
 	});
 	clearTimeout(timer);
-	timer = setTimeout("gotoNextNews()", 3000);
+	timer = setTimeout("gotoNextNews()", 5000);
 }
 
 function gotoNextNews() {
@@ -32,7 +34,19 @@ function gotoPreviousNews() {
 
 function processRemarkPageData(data) {
 	remarkPageNum = data.remarkPages.length;
-	
+	$("#remark-links p").text("第" + (remarkPageCount + 1) + "页，共" + remarkPageNum + "页");
+	$("#remark-session").empty();
+	for (var i = 0; i < data.remarkPages[remarkPageCount].remarks.length; i++)
+	{
+		if (i > 0)
+			$("#remark-session").append($("<hr />"));
+		var remarkDiv = $("<div class='remark-div' />");
+		remarkDiv.append($("<img class='photo' />").attr("src", data.remarkPages[remarkPageCount].remarks[i].photo));
+		remarkDiv.append($("<p class='author' />").text(data.remarkPages[remarkPageCount].remarks[i].author));
+		remarkDiv.append($("<p class='date' />").text(data.remarkPages[remarkPageCount].remarks[i].date));
+		remarkDiv.append($("<p class='remark' />").text(data.remarkPages[remarkPageCount].remarks[i].remark));
+		$("#remark-session").append(remarkDiv);
+	}
 }
 
 function gotoRemarkPage(x) {
@@ -48,18 +62,38 @@ function gotoNextRemarkPage() {
 	if (remarkPageCount < (remarkPageNum - 1))
 		gotoRemarkPage(remarkPageCount + 1);
 	else
-		gotoRemarkPage(0);
+		alert("已是最后一页。");
 }
 
 function gotoPreviousRemarkPage() {
 	if (remarkPageCount > 0)
-		gotoNews(remarkPageCount - 1);
+		gotoRemarkPage(remarkPageCount - 1);
 	else
-		gotoNews(remarkPageNum - 1);
+		alert("已是第一页。");
+}
+
+function gotoLastRemarkPage() {
+	if (remarkPageCount < (remarkPageNum - 1))
+		gotoRemarkPage(remarkPageNum - 1);
+	else
+		alert("已是最后一页。");
+}
+
+function gotoFirstRemarkPage() {
+	if (remarkPageCount > 0)
+		gotoRemarkPage(0);
+	else
+		alert("已是第一页。");
 }
 
 function getReady() {
 	gotoNews(0);
 	gotoRemarkPage(0);
+	$("a#gotoPreviousNews").click(gotoPreviousNews);
+	$("a#gotoNextNews").click(gotoNextNews);
+	$("a#gotoFirstRemarkPage").click(gotoFirstRemarkPage);
+	$("a#gotoPreviousRemarkPage").click(gotoPreviousRemarkPage);
+	$("a#gotoNextRemarkPage").click(gotoNextRemarkPage);
+	$("a#gotoLastRemarkPage").click(gotoLastRemarkPage);
 }
 
